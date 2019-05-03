eaa483a1120f75a6c42e1fff4d0c8e171d1d8529
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2019-05-03 18:18:35 +0200

    Add multiple names generation

--- src/App.js
+++ src/App.js
@@ -13,5 +13,5 @@ const App = () => {
     </div>
   );
 };
-
+ 
 export default App;
--- src/NameGenerator.js
+++ src/NameGenerator.js
@@ -12,93 +12,78 @@ const filterVerbs = words => words.filter(({tags}) => tags && tags.includes("v")
 
 const capitalize = name => name.charAt(0).toUpperCase() + name.slice(1);
 
-const generateNofN = async ({letter}, onSuccess, onFailure) => {
-  try {
-    const firstWords = await getStartingWith(letter);
-    const potentialFirst = extractWords(filterNouns(firstWords));
-    const first = randomElement(potentialFirst);
+const generateNofN = async ({letter}) => {
+  const firstWords = await getStartingWith(letter);
+  const potentialFirst = extractWords(filterNouns(firstWords));
+  const first = randomElement(potentialFirst);
+  
+  if (!first) throw("Could not find name");
+  
+  const secondWords = await getRelated(first);
+  const potentialSecond = extractWords(filterNouns(secondWords));
+  const second = randomElement(potentialSecond);
+  
+  if (!second) throw("Could not find name");
 
-    if (!first) throw("Could not find name");
-
-    const secondWords = await getRelated(first);
-    const potentialSecond = extractWords(filterNouns(secondWords));
-    const second = randomElement(potentialSecond);
-
-    if (!second) throw("Could not find name");
-
-    onSuccess([`${capitalize(first)} of ${capitalize(second)}`]);
-  } catch(err) {
-    onFailure(err);
-  }
+  return `${capitalize(first)} of ${capitalize(second)}`;
 };
 
 const generateAdjNoun = async ({letter}, onSuccess, onFailure) => {
-  try {
-    const firstWords = await getStartingWith(letter);
-    const potentialFirst = extractWords(filterAdjectives(firstWords));
-    const first = randomElement(potentialFirst);
-
-    if (!first) throw("Could not find name");
-
-    const secondWords = await getMatchingNoun(first);
-    const potentialSecond = extractWords(filterNouns(secondWords));
-    const second = randomElement(potentialSecond);
-
-    if (!second) throw("Could not find name");
-
-    onSuccess([`${capitalize(first)} of ${capitalize(second)}`]);
-  } catch(err) {
-    onFailure(err);
-  }
+  const firstWords = await getStartingWith(letter);
+  const potentialFirst = extractWords(filterAdjectives(firstWords));
+  const first = randomElement(potentialFirst);
+  
+  if (!first) throw("Could not find name");
+  
+  const secondWords = await getMatchingNoun(first);
+  const potentialSecond = extractWords(filterNouns(secondWords));
+  const second = randomElement(potentialSecond);
+  
+  if (!second) throw("Could not find name");
+  
+  return `${capitalize(first)} of ${capitalize(second)}`;
 };
 
 const generateVerbNoun = async ({letter}, onSuccess, onFailure) => {
-  try {
-    const firstWords = await getStartingWith(letter);
-    const potentialFirst = extractWords(filterVerbs(firstWords));
-    const first = randomElement(potentialFirst);
-
-    if (!first) throw("Could not find name");
-
-    const secondWords = await getRelated(first);
-    const potentialSecond = extractWords(filterNouns(secondWords));
-    const second = randomElement(potentialSecond);
-
-    if (!second) throw("Could not find name");
-
-    onSuccess([capitalize(first) + " " + capitalize(second)]);
-  } catch(err) {
-    onFailure(err);
-  }
+  const firstWords = await getStartingWith(letter);
+  const potentialFirst = extractWords(filterVerbs(firstWords));
+  const first = randomElement(potentialFirst);
+  
+  if (!first) throw("Could not find name");
+  
+  const secondWords = await getRelated(first);
+  const potentialSecond = extractWords(filterNouns(secondWords));
+  const second = randomElement(potentialSecond);
+  
+  if (!second) throw("Could not find name");
+  
+  return capitalize(first) + " " + capitalize(second);
 };
 
 const generateNoun = async ({letter}, onSuccess, onFailure) => {
-  try {
-    const firstWords = await getStartingWith(letter);
-    const potentialFirst = extractWords(filterNouns(firstWords));
-    const first = randomElement(potentialFirst);
+  const firstWords = await getStartingWith(letter);
+  const potentialFirst = extractWords(filterNouns(firstWords));
+  const first = randomElement(potentialFirst);
+  
+  if (!first) throw("Could not find name");
+  return capitalize(first);
+};
 
-    if (!first) throw("Could not find name");
-    onSuccess([capitalize(first)]);
-  } catch(err) {
-    onFailure(err);
+const generateRandom = ({letter}) => {
+  switch (Math.floor(Math.random()*4)) {
+  case 0:
+    return generateNofN({letter});
+  case 1:
+    return generateAdjNoun({letter});
+  case 2:
+    return generateVerbNoun({letter});
+  case 3:
+  default:
+    return generateNoun({letter});
   }
 };
 
-export const generate = ({letter}, onSuccess, onFailure) => {
-  switch (Math.floor(Math.random()*4)) {
-  case 0:
-    generateNofN({letter}, onSuccess, onFailure);
-    break;
-  case 1:
-    generateAdjNoun({letter}, onSuccess, onFailure);
-    break;
-  case 2:
-    generateVerbNoun({letter}, onSuccess, onFailure);
-    break;
-  case 3:
-  default:
-    generateNoun({letter}, onSuccess, onFailure);
-    break;
-  }
+export const generate = ({letter, count = 10}, onSuccess, onFailure) => {
+  const promises = [...Array(count).keys()].map(() => generateRandom({letter}));
+  Promise.all(promises).then(onSuccess).catch(onFailure);
 };
